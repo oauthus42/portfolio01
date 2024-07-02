1c7673c01d6a994feff720985f8174b2e9544d4d
Oauthus42 <var.verdandi@gmail.com>
2024-07-02 23:59:23 +0300

    Подключение к MondoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 import express from "express";
+import dotenv, { config } from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import { connect } from "http2";
+import connectMongoDB from "./db/connectMongoDB.js";
 
+dotenv.config();
 const app = express();
-app.use("/api/auth", authRoutes);
 
+console.log(process.env.mongoURI);
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
 app.listen(8000, () => {
-    console.log("express server working")
+    console.log(`server is runing port ${PORT}`);
+    connectMongoDB();
 });
