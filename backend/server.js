25ac7a06ee354f7e8303a4a3eeb40dd5d9efde0b
Oauthus42 <var.verdandi@gmail.com>
2024-07-03 00:29:50 +0300

    Revert "Подключение к MongoDB + рефакторинг"
    This reverts commit 1a847408de4f8ac3a244d1ea6fc0b5aa0e114411.

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,12 @@ import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 const app = express();
+
+console.log(process.env.mongoURI);
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
-app.listen(PORT, () => {
+app.listen(8000, () => {
     console.log(`server is runing port ${PORT}`);
     connectMongoDB();
 });
