195d4edd3587defcea1679e0a5f0e5f3c2c09610
Oauthus42 <var.verdandi@gmail.com>
2024-07-02 22:15:05 +0300

    AUTH routes and controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+export const sugnup = async (req, res) => {
+        res.json ({
+            data: "signup endpoint",
+        });
+};
+
+export const login = async (req, res) => {
+    res.json ({
+        data: "login endpoint",
+    });
+};
+
+export const logout = async (req, res) => {
+    res.json ({
+        data: "logout endpoint",
+    });
+};
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('server is running');
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+app.use("/api/auth", authRoutes);
+
+app.listen(8000, () => {
+    console.log("express server working")
+});
