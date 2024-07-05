8fd16608af1f42cba9f95fd08a438996cef11605
Oauthus42 <var.verdandi@gmail.com>
2024-07-05 22:08:49 +0300

    все лайкнутые посты

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -151,5 +151,27 @@ export const getLikePosts = async(req, res) => {
     } catch (error) {
         console.log('Ошибка в getLikePosts (post.controller): ', error);
         res.status(500).json({message:'Internal Server Error'});
-    }
+    };
+};
+
+export const getFollowingPost = async(req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({message:'Пользователь не найден'});
+
+        const following = user.following;
+        const feedPosts = await Post.find({user:{$in: following}}).sort({createdAt: -1}).populate({
+            path: 'user',
+            select: '-password'
+            }).populate({
+            path:'comment',
+            select:'-password'
+        });
+
+        res.status(200).json(feedPosts);
+    } catch (error) {
+        console.log('Ошибка в getFollowingPost (post.controller): ', error);
+        res.status(500).json({message:'Internal Server Error'});
+    };
 };
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { commentOnPost, createPost, deletePost, getAllPost, getLikePosts, likeUnlikePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost, 
+        getAllPost, getFollowingPost, getLikePosts, 
+        likeUnlikePost 
+    } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/all", protectRoute, getAllPost);
+router.get("/all", protectRoute, getAllPost);
+router.get("/following", protectRoute, getFollowingPost);
 router.get("/likes/:id", protectRoute, getLikePosts);
 router.post("/create", protectRoute, createPost);
 router.delete("/delete/:id", protectRoute, deletePost);
