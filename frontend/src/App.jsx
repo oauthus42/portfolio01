6e126f757564b58c417a1c789fd048497e6672ee
Oauthus42 <var.verdandi@gmail.com>
2024-07-07 22:00:36 +0300

    страница регистрации

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/auth/login/Login';
+import Home from './pages/home/Home';
+import Signup from './pages/auth/signup/Signup';
 
 const App = () => {
-
-
-
-
   return (
-    <>
-    <h1>привет от ui (app.jsx)</h1>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+    
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/notifications' element={<Notification />}/>
+        <Route path='/profile/:username' element={<Profile />}/>
+      </Routes>
+    </div>
   )
   };
 
