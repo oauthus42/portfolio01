77b53e59d4b03067d15e16ccfc8d4574956edfb4
Oauthus42 <var.verdandi@gmail.com>
2024-07-13 23:36:21 +0300

    ui. обновление профиля

---
+++ frontend/src/pages/profile/EditProfile.jsx
@@ -0,0 +1,127 @@
+import { useEffect, useState } from "react";
+//на третьем этапе:
+//import useUpdateUserProfile from "../../hooks/useUpdateUserProfile";
+
+const EditProfile = ({ authUser }) => {
+	const [formData, setFormData] = useState({
+		fullName: "",
+		username: "",
+		email: "",
+		bio: "",
+		link: "",
+		newPassword: "",
+		currentPassword: "",
+	});
+
+	const { updateProfile, isUpdatingProfile } = useUpdateUserProfile();
+
+	const handleInputChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	useEffect(() => {
+		if (authUser) {
+			setFormData({
+				fullName: authUser.fullName,
+				username: authUser.username,
+				email: authUser.email,
+				bio: authUser.bio,
+				link: authUser.link,
+				newPassword: "",
+				currentPassword: "",
+			});
+		}
+	}, [authUser]);
+
+	return (
+		<>
+			<button
+				className='btn btn-outline rounded-full btn-sm'
+				onClick={() => document.getElementById("edit_profile_modal").showModal()}
+			>
+				Обновить профиль
+			</button>
+			<dialog id='edit_profile_modal' className='modal'>
+				<div className='modal-box border rounded-md border-gray-700 shadow-md'>
+					<h3 className='font-bold text-lg my-3'>Обновить профиль</h3>
+					<form
+						className='flex flex-col gap-4'
+						onSubmit={(e) => {
+							e.preventDefault();
+							updateProfile(formData);
+						}}
+					>
+						<div className='flex flex-wrap gap-2'>
+							<input
+								type='text'
+								placeholder='Фамилия'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.fullName}
+								name='fullName'
+								onChange={handleInputChange}
+							/>
+							<input
+								type='text'
+								placeholder='Имя'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.username}
+								name='username'
+								onChange={handleInputChange}
+							/>
+						</div>
+						<div className='flex flex-wrap gap-2'>
+							<input
+								type='email'
+								placeholder='E-mail'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.email}
+								name='email'
+								onChange={handleInputChange}
+							/>
+							<textarea
+								placeholder='О себе'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.bio}
+								name='bio'
+								onChange={handleInputChange}
+							/>
+						</div>
+						<div className='flex flex-wrap gap-2'>
+							<input
+								type='password'
+								placeholder='Текущий пароль'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.currentPassword}
+								name='currentPassword'
+								onChange={handleInputChange}
+							/>
+							<input
+								type='password'
+								placeholder='Новый пароль'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.newPassword}
+								name='newPassword'
+								onChange={handleInputChange}
+							/>
+						</div>
+						<input
+							type='text'
+							placeholder='Ссылка'
+							className='flex-1 input border border-gray-700 rounded p-2 input-md'
+							value={formData.link}
+							name='link'
+							onChange={handleInputChange}
+						/>
+						<button className='btn btn-primary rounded-full btn-sm text-white'>
+							{isUpdatingProfile ? "Updating..." : "Update"}
+						</button>
+					</form>
+				</div>
+				<form method='dialog' className='modal-backdrop'>
+					<button className='outline-none'>Закрыть</button>
+				</form>
+			</dialog>
+		</>
+	);
+};
+export default EditProfile;
--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -219,4 +219,4 @@ const Profile = () => {
 		</>
 	);
 };
-export default ProfilePage;
+export default Profile;
